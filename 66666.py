9adb9b53704f2ea6e9593ce3f7a87050995a661f
Aleksey Vorobev <daffernn@mail.ru>
2021-10-08 22:55:23 +0300

    add new file

---
+++ 66666.py
@@ -0,0 +1,27 @@
+min=int(99999999999)
+f=int(0)
+count=int(0)
+print("Введите длину массива")
+try:
+    x=int(input())
+    array=[0]*x
+    for i in range(x):
+        array[i]=int(input("Введите число "))
+    print("Массив",array)
+    print("Введите дельту")
+    delta=int(input())
+except ValueError:
+    print("Incorrect value")
+    exit()
+else:
+        for i in range(x):
+            if array[i]< min:
+                min=array[i]
+for i in range(x):
+    if (array[i]-min==delta) | (array[i]+min==delta):
+        f=1
+        count+=1
+if f==0:
+    print("Нет подходящих элементов или дельта является отрицательной")
+else:
+    print("Количество элементов удволетворяющих условию", count)    
